fc81e3e06df70265f9324f9f9833860f98377c8f
usman <imdevusman@gmail.com>
2025-03-03 09:52:35 +0500

    `Refactor Player component to allow editing of player names`

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/Components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/Components/Player.jsx
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
 
-function Player({ name, symbol }) {
+function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   const handleEditClick = () => {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   };
 
-  let PlayerName = <span className="player-name">{name}</span>;
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
   if (isEditing) {
-    PlayerName = <input type="text" required />;
+    editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required />;
   }
 
   return (
-    <>
-      <li>
-        <span className="player">
-          {PlayerName}
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={handleEditClick}>Edit</button>
-      </li>
-    </>
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
   );
 }
 
