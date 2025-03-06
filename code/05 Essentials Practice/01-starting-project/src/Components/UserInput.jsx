402be995e39898d3190ef7d49718e470aa425613
usman <imdevusman@gmail.com>
2025-03-06 14:25:33 +0500

    `Update UserInput component to use state and handle user input changes`

--- code/05 Essentials Practice/01-starting-project/src/Components/UserInput.jsx
+++ code/05 Essentials Practice/01-starting-project/src/Components/UserInput.jsx
@@ -1,32 +1,74 @@
-import React from "react";
+import { useState } from "react";
 
 function UserInput() {
-  return (
-    <>
-      <section id="user-input">
-        <div className="input-group">
-          <p>
-            <label htmlFor="">Initial Investment</label>
-            <input type="number" required />
-          </p>
-          <p>
-            <label htmlFor="">Annual Investment</label>
-            <input type="number" required />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="">Expected Return</label>
-            <input type="number" required />
-          </p>
-          <p>
-            <label htmlFor="">Duration</label>
-            <input type="number" required />
-          </p>
-        </div>
-      </section>
-    </>
-  );
+    const [UserInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue, 
+            };
+        });
+    }
+
+    return (
+        <>
+            <section id="user-input">
+                <div className="input-group">
+                    <p>
+                        <label htmlFor="">Initial Investment</label>
+                        <input
+                            type="number"
+                            required
+                            value={UserInput.initialInvestment}
+                            onChange={(event) =>
+                                handleChange("initialInvestment", event.target.value)
+                            }
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor="">Annual Investment</label>
+                        <input
+                            type="number"
+                            value={UserInput.annualInvestment}
+                            onChange={(event) =>
+                                handleChange("annualInvestment", event.target.value)
+                            }
+                            required
+                        />
+                    </p>
+                </div>
+                <div className="input-group">
+                    <p>
+                        <label htmlFor="">Expected Return</label>
+                        <input
+                            type="number"
+                            value={UserInput.expectedReturn}
+                            onChange={(event) =>
+                                handleChange("expectedReturn", event.target.value)
+                            }
+                            required
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor="">Duration</label>
+                        <input
+                            type="number"
+                            value={UserInput.duration}
+                            onChange={(event) => handleChange("duration", event.target.value)}
+                            required
+                        />
+                    </p>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default UserInput;
