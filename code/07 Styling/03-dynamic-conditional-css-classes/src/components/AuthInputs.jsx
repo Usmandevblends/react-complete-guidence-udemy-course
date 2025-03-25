6dfdf40233cdb26bbc6673691e1b67e268c72ea8
usman <imdevusman@gmail.com>
2025-03-25 13:04:29 +0500

    main

--- code/07 Styling/03-dynamic-conditional-css-classes/src/components/AuthInputs.jsx
+++ code/07 Styling/03-dynamic-conditional-css-classes/src/components/AuthInputs.jsx
@@ -29,9 +29,9 @@ export default function AuthInputs() {
           </label>
           <input
             type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            // }}
+            style={{
+              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            }}
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
