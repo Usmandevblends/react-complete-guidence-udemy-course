857a8d4a64eb2c9f423bfcb72a94b65d12f23866
usman <imdevusman@gmail.com>
2025-03-26 13:32:24 +0500

    `Added state management and event handling to Player component`

--- code/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [isNameSubmitted, setIsNameSubmitted] = useState(false);
+
+  /**
+   * Handles changes to the input field.
+   * @param {ChangeEvent} event - The event triggered by the input field change.
+   */
+  const handleInputChange = (event) => {
+    setIsNameSubmitted(false);
+    setPlayerName(event.target.value);
+    console.log(setPlayerName)
+  };
+
+  /**
+   * Handles the submission of the player's name.
+   */
+  const handleSubmit = () => {
+    setIsNameSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome{" "}
+        {isNameSubmitted ? playerName : "unknown entity"}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          value={playerName}
+          onChange={handleInputChange}
+          placeholder="Enter your name"
+        />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
