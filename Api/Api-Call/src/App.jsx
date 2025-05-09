51baee293e7e1c4b8f32784093bbe07630ee3d24
usman <imdevusman@gmail.com>
2025-05-09 13:07:56 +0500

    `Added CORS and Express dependencies, updated package-lock.json and package.json, and modified App.css and App.jsx to display product list from API.`

--- Api/Api-Call/src/App.jsx
+++ Api/Api-Call/src/App.jsx
@@ -3,40 +3,33 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+    fetch('https://dummyjson.com/products')
       .then((res) => res.json())
       .then((response) => {
-        console.log('API Response:', response);
-        setData(response);
-        setTimeout(() => {
-        setLoading(false);
-          },3000);
+        setData(response.products); 
       });
   }, []);
 
   return (
     <div className="App">
-      <h1 style={{ textAlign: 'center' }}>User List from API</h1>
-      <ul style={{ listStyleType: 'none' }}>
-        {loading
-          ? Array.from({ length: 5 }).map((_, index) => (
-              <li key={index} className="skeleton-card">
-                <div className="skeleton skeleton-title"></div>
-                <div className="skeleton skeleton-text"></div>
-                <div className="skeleton skeleton-text short"></div>
-              </li>
-            ))
-          : data.map((item) => (
-              <li key={item.id} className="comment-card">
-                <p><strong>Name:</strong> {item.name}</p>
-                <p><strong>Email:</strong> {item.email}</p>
-                <p><strong>Comment:</strong> {item.body}</p>
-              </li>
-            ))}
-      </ul>
+      <h1 className="title">Product List from API</h1>
+
+      <div className="product-container">
+        {data.map((item) => (
+          <div className="card-container" key={item.id}>
+            <div className="card">
+              <img className="card-image" src={item.image} alt={item.title} />
+              <div className="card-details">
+                <h2 className="card-title">{item.title}</h2>
+                <p className="card-description">{item.description}</p>
+                <p className="card-price">${item.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
